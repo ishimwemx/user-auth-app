ce562a05f5d8416a95511eb5d9f0cb5b836cd0dc
ishimwemx <maximeishimw3@gmail.com>
2025-05-30 12:10:18 +0200

    Add background

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -20,7 +20,7 @@ function SignIn() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen fixed inset-0 bg-[url('../bg.jpg')] bg-cover bg-center blur-sm p-4">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Login
