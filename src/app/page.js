2f9aada6e4db8817caea293486fdc4425eabac36
ishimwemx <maximeishimw3@gmail.com>
2025-05-30 12:37:44 +0200

    Updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { fetchAuthUserAction } from "@/actions";
 import Logout from "@/components/log-out";
 import { redirect } from "next/navigation";
@@ -9,7 +10,7 @@ export default async function Home() {
   if (!currentUser?.success) redirect("/sign-in");
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen fixed inset-0 bg-[url('/bg.jpg')] bg-cover bg-center blur-sm p-4">
+    <main className="flex flex-col items-center justify-center min-h-screen fixed inset-0 bg-[url('/bg.jpg')] bg-cover bg-center blur-sm p-4">
       <div className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md text-center z-10">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">
           Welcome
@@ -26,6 +27,6 @@ export default async function Home() {
           <p className="text-gray-600">No user is currently logged in.</p>
         )}
       </div>
-    </div>
+    </main>
   );
 }
